7cbe7af3b003957dc43b33376ff6e9b367ccd348
rakeshdatti <rakeshdatti05@>
2025-07-10 23:01:33 +0530

    script-files

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=RAKESH
+PERSON2=SURESH
+
+echo "Hello, $PERSON1 and $PERSON2!"
+echo "{$PERSON2}:: is a good friend of $PERSON1."
+echo "I am $PERSON1, and I am a software engineer."
