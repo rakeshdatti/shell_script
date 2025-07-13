627de4a5364a5f100cc6028859c6f0dea7f6eed7
rakeshdatti <rakeshdatti05@>
2025-07-13 12:30:02 +0530

    added.sh

--- 06-install-mysql.sh
+++ 06-install-mysql.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERror you have to be sudo user"
+    exit 1
 fi
 
-dnf install mysql-community-server -y
+yum install mysql-server -y
 if [$? -eq 0 ]
 then
     echo "mysql is installed sucessfully"
+    exit 1
 else
-    echo "mysql is not isntalled "
+    echo "mysql is not installed "
 fi 
