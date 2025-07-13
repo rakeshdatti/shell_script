dd086fb1d11507f70f4a7d39248f899051da5da2
rakeshdatti <rakeshdatti05@>
2025-07-13 13:19:19 +0530

    added.sh

--- 08-colors.sh
+++ 08-colors.sh
@@ -6,10 +6,10 @@ USERID=$(id -u)
 Validate() {
     if [ $1 -ne 0 ]
     then 
-        echo -e "\e[31$2..failed"
+        echo -e "\e[31m$2..failed"
         exit 1
     else
-        echo -e "\e[32$2..success"
+        echo -e "\e[32m$2..success"
     fi
 }
 if [ $USERID -ne 0 ]
@@ -23,6 +23,6 @@ then
     dnf install mysql -y
     Validate $? "Installing mysql..."
 else
-    echo -e "\e[33MySQL is already installed"
+    echo -e "\e[33mMySQL is already installed"
     exit 0
 fi
